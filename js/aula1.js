d189e50b29be7afb350652c0f73dd1d860bacf52
MrSalis <philip.cost@hotmail.com>
2023-07-21 01:46:19 -0300

    Add pasta de aulas JS

---
+++ js/aula1.js
@@ -0,0 +1,14 @@
+/*
+-Para rodar o código e suas respectivas alterações direto no pc, precisa ter instalado:
+
+>NODE
+>CODE RUNNER (estenção VScode)
+>ctrl+alt+n (P visualizar as alterações)
+
+*/
+
+console.log("testando o console do js")
+console.log("testando novamebnte")
+
+
+
