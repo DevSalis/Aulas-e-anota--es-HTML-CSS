fb7d8d297b94a13c5f854466d7680d826bbb28d7
MrSalis <philip.cost@hotmail.com>
2023-07-22 04:59:51 -0300

    aula-sobre-variáveis

---
+++ js/aulas.js
@@ -0,0 +1,26 @@
+/*
+-Para rodar o código e suas respectivas alterações direto no pc, precisa ter instalado:
+
+>NODE
+>CODE RUNNER (estenção VScode)
+>ctrl+alt+n (P visualizar as alterações)
+
+*/
+
+console.log("testando o console do js")
+console.log("testando novamebnte")
+
+/*
+VARIAVEIS => Um espaço de memoria (como uma pasta reservada) para armasenar valores (textos, numeros, imagens)
+            cada variavel tem um nome defido pelo programador, de acordo com os valores que serão armazenados.
+            para executar uma variavel, usa-se palavras chaves, que apesar de ativar a variavel,
+            as palaavras chaves yeem caracteristicas defiferentes, são elas:
+
+let - permite trocar o valor da variavel a qualquer momento, se for do interesse do programador.
+const - o valor é constante, por tanto não pode sofrer alterações
+var - fora de uso
+*/
+
+const abacate = 40
+
+console.log(abacate)
