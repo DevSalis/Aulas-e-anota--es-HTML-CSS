1e3b281199cc962e10eecda45954e4d5aa8c4a2b
MrSalis <philip.cost@hotmail.com>
2023-09-08 19:29:20 -0300

    Aula-addEventListener-and-add-select-no-html

--- js/aulas_2.js
+++ js/aulas_2.js
@@ -128,6 +128,21 @@ const dentroDoInput = document.querySelector("#main-input")
 const newParagrafo = document.querySelector(".valorDoInput")
 
 function cliqueiNoBotao(){
-        (newParagrafo.innerHTML = dentroDoInput.value)
-
+        console.log(newParagrafo.innerHTML = dentroDoInput.value)
 }
+
+/*
+EddEventListener => Ele permite que você especifique uma função que será executada
+                quando um determinado evento ocorrer no elemento selecionado.
+                Ex: Você pode usar o addEventListener para adicionar um ouvinte de clique a um botão
+                e executar uma função quando o botão for clicado.
+                Isso permite que você crie interatividade em seu site ou aplicativo web.
+*/
+
+//ex:
+
+const seletor = document.querySelector("select")
+
+seletor.addEventListener("change", function(event){
+        console.log(event)
+})
