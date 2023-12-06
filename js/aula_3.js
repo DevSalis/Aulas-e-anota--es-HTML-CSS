e86be147faf4b5bc82f72e1808997658bbd87120
MrSalis <philip.cost@hotmail.com>
2023-12-06 01:37:18 -0300

    operadores-aritmaticos

--- js/aula_3.js
+++ js/aula_3.js
@@ -4,21 +4,42 @@ Math => Matematica
 OBS: para execultar o console.log na saida do terminal, vc deve salvar as alterações antes
         e depois teclar o atalho CTRL + ALT + N
 
-pow => É usado para calculos de POTENCIA, ex: ao quadrado, ao cubo...
+        pow => É usado para calculos de POTENCIA, ex: ao quadrado, ao cubo...
         ex: Math.pow(2,2) ou seja, 2² / Math.pow(2,3) seria 2³
 
-sqrt => É usado para cauculos de raiz quadrada ou ao cubo
+        sqrt => É usado para cauculos de raiz quadrada ou ao cubo
         ex: Math.sqrt(25,2) ou seja, √25
 
-ceil => Arredonda a pra cima (a fração deve ser separada por . e n por , ex: 7.98)
+        ceil => Arredonda a pra cima (a fração deve ser separada por . e n por , ex: 7.98)
 
-floor => Arredonda pra baixo
+        floor => Arredonda pra baixo
 
-random => Mostra numero aleatório entre zero e 1
+        random => Mostra numero aleatório entre zero e 1
 */
 
-const result = Math.random()
+const result = 12 % 2
 console.log(result)
+
+/*
+Operadores Aritméticos
+
++ => Adição
+- => Subtração
+/ => Divisão
+* => Multiplicação
+
+% => Resto (mostra apenas a sobra do calculo, ex: 13/2 sobraria 1, ou seja, nesse caso 1 é o sesultado.)
+++ => Incremento (soma-se 1 a mais)
+-- => Decremento (subtrai 1 a menos)
+** => Exponencial (tem praticamente a mesma função do POW porem duas vezes mais)
+*/
+
+/*
+Operadores Atribuição
+
+
+*/
+
 
 
 
