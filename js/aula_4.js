94884e840eedcba44ed545a0c278999b86234bfc
MrSalis <philip.cost@hotmail.com>
2023-12-17 02:12:21 -0300

    praticando-for-length

--- js/aula_4.js
+++ js/aula_4.js
@@ -1,22 +1,23 @@
 /*
 "for" é utilizada para criar um loop, ou seja, para repetir um bloco de código várias vezes.
-O laço "for" é composto por três partes:
+A estrutura do "for" é composto por três partes:
 
-Inicialização => Criar uma variavel e um valor (é comum essa variavel, nesse caso especifico ser chamada de "i");
-Condição de continuação => Cofiguração pra essa variavel manter a execussão, enquanto essa condição for true (verdadeira).
-Incremento ou expressão final => Configuração do que deve acontecer sempre que o laço acontcer.
+Inicialização => Criar uma variável e um valor (é comum essa variável, nesse caso especifico ser chamada de "i");
+
+Condição de continuação => Definição até quando o loop deve repetir, uma vez que essa condição seja true (verdadeira).
+
+Incremento ou expressão final => Configuração do que deve acontecer sempre que o laço encerrar.
 
 ex: for ([Inicialização]; [Condição de continuação]; [Incremento ou expressão final]) {
-    seu codigo aqui
+    Seu código aqui
 }
 
-obs: O LENGTH é usado para definir o tamanho de um string, seja por caracteres ou elementos dentro uma array.
-    (a array é o nome que damos a variavel que armazena varios elementos,
-    como uma sequencia numerica, ou lista de usuarios)
-    
+obs: O LENGTH é usado para definir o tamanho de uma string, ou seja, os elementos dentro uma array.
+    (A array é o nome que damos a variável que armazena vários elementos,
+    Como uma sequência numérica, ou lista de usuários)
 */
 
-let family = [1,2,3,4,5,6,7,8]
+let family = ["Silas", "Rute", "Ruan", "Ladja"]
 
 for (let i = 0; i < family.length; i++) {
     console.log(family[i])
