01686d5cd07e77aee4ec873f9b681bc5bedf72a1
MrSalis <philip.cost@hotmail.com>
2025-01-06 01:15:53 -0300

    Query, Route e Body Params

--- node/server.js
+++ node/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 const app = express()
 
-app.get('/usuario', (req, res) => {
+app.use(express.json())
+
+app.get('/usuario/:abacate2', (req, res) => {
+    console.log(req)
     res.send("Meu back-end esta fincionando")
 })
 
 app.post('/usuario', (req, res) => {
+    console.log(req)
     res.send("rota post ok")
 })
 
