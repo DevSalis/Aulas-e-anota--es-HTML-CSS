13130c9d20c7880632e0ec94a1347bb57aba1054
MrSalis <philip.cost@hotmail.com>
2025-04-28 17:53:54 -0300

    treinando Query Params e Route Params

--- node/server.js
+++ node/server.js
@@ -1,20 +1,20 @@
 import express from "express";
-const app = express()
-const user = []
+const app = express();
+const user = [];
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/usuario', (req, res) => {
-    
-    res.status(200).json(user)
-})
+app.get("/usuario/:abacate", (req, res) => {
+  console.log(req);
+  res.status(200).json({ mensagem: "teste get" });
+});
 
-app.post('/usuario', (req, res) => {
-    user.push(req.body)
-    res.status(201).json({message: "Usuário cadastrado com sucesso"})
-})
+app.post("/usuario", (req, res) => {
+  user.push(req.body);
+  res.status(201).json({ message: "Usuário cadastrado com sucesso" });
+});
 
-app.listen(3000)
+app.listen(3000);
 
 /*
 username mongoDb: philipcost
